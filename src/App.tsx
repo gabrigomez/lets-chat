423d381a9eda4f801d081eeb0d31f5f43a2e176a
Gabriel <gabrigomez15@gmail.com>
2023-03-14 16:12:32 -0300

    feat: start app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
+//import * as io from "socket.io-client";
+import { useState } from "react";
+
+//const socket = io.connect("http://localhost:3000");
 
 function App() {
+  const [user, setUser] = useState("");
+  const [room, setRoom] = useState("");
+  
+  const joinRoom = () => {
+    if (user !== "" && room !== "") {
+      //socket.emit("enter_room", room);
+      console.log('clicked')      
+    }
+  };
+  
   return (
-    <div className="h-screen bg-cyan-800">            
+    <div className="h-screen flex flex-col p-2 items-center bg-cyan-400">
+      <h1 className='text-xl mb-6'>
+        Lets chat!
+      </h1>
+      <div>
+          <input
+            type="text"
+            placeholder="Your Name"
+            className='mr-4'
+            onChange={(event) => {
+              setUser(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            className='mr-4'
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button className='bg-blue-400 p-2 rounded-full hover:bg-blue-500 duration-200' onClick={joinRoom}>
+            Enter
+          </button>            
+      </div>
     </div>
   );
 }
