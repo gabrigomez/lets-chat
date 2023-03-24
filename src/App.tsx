93ab779282da5963d0e499311b67b70305d22c3f
Gabriel <gabrigomez15@gmail.com>
2023-03-24 15:51:53 -0300

    fix: try to fix cors in client side

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Chat } from './components/Chat';
 
 import { SiWechat } from 'react-icons/si';
 
-const socket = io.connect("https://lets-chat-api.vercel.app/");
+const socket = io.connect("https://lets-chat-api.vercel.app/", { transports : ['websocket'] });
 
 function App() {
   const [user, setUser] = useState("");
